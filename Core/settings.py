4807c2e41877164cb96ed203ecd61f2152e5b0b6
danielluccio <daniel130877@gmail.com>
2025-05-26 15:33:25 -0300

    🔧 Feat(django-jazzmin -> painel de admin do django)

--- Core/settings.py
+++ Core/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "jazzmin", # Estilização de visual do admin
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
